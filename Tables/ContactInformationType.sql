e2542e18c8c5ec590c533c5e45f59191a1fc5e95
ShariMaes <sharimaes@hotmail.com>
2023-01-31 11:29:03 +0100

    Update DB + added logo's

---
+++ Tables/ContactInformationType.sql
@@ -0,0 +1,13 @@
+﻿CREATE TABLE [dbo].[ContactInformationType]
+(
+	[ContactInformationTypeCodeId] INT NOT NULL IDENTITY(1,1), 
+    [ContactInformationTypeCaption] NVARCHAR(255) NULL, 
+    [ResourceId] Uniqueidentifier NOT NULL ,
+    [Created] DATETIME2 NULL, 
+    [CreatedBy] NVARCHAR(255) NULL, 
+    [Modified] DATETIME2 NULL, 
+    [ModifiedBy] NVARCHAR(255) NULL, 
+    CONSTRAINT [AK_ContactInformationType_Id] PRIMARY KEY ([ContactInformationTypeCodeId]), 
+    CONSTRAINT [FK_ContactInformationType_Resource] FOREIGN KEY (ResourceId) REFERENCES [Resource]([ResourceId])
+    
+)
